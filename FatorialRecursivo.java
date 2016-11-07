e0b6615c34f90b69f1f6b52be28def529134655f
Dalton Leonardo <Dalton Leonardo>
2016-11-06 23:22:18 -0200

    fatorial recursivo implementado

---
+++ FatorialRecursivo.java
@@ -0,0 +1,11 @@
+public class FatorialRecursivo{
+	public statis int calcularFatorialRecursivo(int n) throws IllegalArgumentException {
+		if( n == 0)
+			return 1;
+		return n * calcularFatorialRecursivo(n-1)
+	}
+	public satic void(String[] arqs){
+	     system.out.println(calcularFatorialRecursivo(5));		
+	}
+}
+
